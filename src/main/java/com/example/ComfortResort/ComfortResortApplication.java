67ba368c368d3661143df398bb56558edfcab161
blagojevic3 <veliborblagojevic88@gmail.com>
2024-10-10 00:42:08 +0200

    local properties initial setupp

---
+++ src/main/java/com/example/ComfortResort/ComfortResortApplication.java
@@ -0,0 +1,13 @@
+package com.example.ComfortResort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfortResortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComfortResortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ComfortResort/ComfortResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ComfortResort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfortResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
